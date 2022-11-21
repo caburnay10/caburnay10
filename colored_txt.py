84989f26f1199fdf61392549e8a5b44e939e9592
caburnay10 <77877968+caburnay10@users.noreply.github.com>
2022-11-21 16:47:53 +0800

    Add files via upload

---
+++ colored_txt.py
@@ -0,0 +1,12 @@
+import os, time
+
+
+while True:
+    
+    color=['color 01', 'color 02','color 03','color 04','color 05','color 06','color 07','color 08','color 09']
+    for i in color:
+        time.sleep(1)
+        os.system(i)
+        os.system('cls') 
+        print('COLOR CHANGING TEXT')
+    
